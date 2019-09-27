be46ee4f29f2571e44cd1fef888b1fa87f44b608
Eric Wells <ewells@ualberta.ca>
2019-09-27 00:16:33 -0600

    inverse kinematics symbolic and numerical added

--- Corke/axisInverseKinematics.m
+++ Corke/axisInverseKinematics.m
@@ -27,7 +27,31 @@ theta5 = [0, 0, 0];
 Tsb5 = Tsbgen5000(theta5);
 Tas5 = [Rgamma(72*4/180*pi), [-100*sin(72*4/180*pi); 100*cos(72*4/180*pi); 0];[0, 0, 0], 1];
 Tab5 = Tas5*Tsb5;
+% 
+% % Symbolic Inverse Kinematics
+% syms t1 t2 t3 real
+% theta1 = [t1, t2, t3];
+% Tsb1 = Tsbgen5000(theta1);
+% Tas1 = [Rgamma(0), [0; 100; 0];[0, 0, 0], 1];
+% Tab1 = Tas1*Tsb1;
+% 
+% syms x y z real
+% e1 = x == Tab1(1, 4);
+% e2 = y == Tab1(2, 4);
+% e3 = z == Tab1(3, 4);
+% 
+% S = solve([e1, e2, e3], [t1, t2, t3]);
 
+% Numerical Inverse Kinematics
+theta0 = [1, 1, 1];
+pdes = [0; 100; 100];
+fkin = @(theta)(AxisReloadedPoseCalc(0, theta));
+fpos = @(T)T(1:3, 4);
 
+err = @(theta) norm(fpos(fkin(theta)) - pdes);
+options = optimset('TolFun', 0.1);
+tic;
+theta = fminsearch(err, theta0, options);
+toc
 
 
