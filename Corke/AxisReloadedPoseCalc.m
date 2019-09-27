4251674a1e94cd41e132d7aa5f39a95816a41c84
MarkSherstan <mshersta@ualberta.ca>
2019-09-27 15:39:18 -0600

    Confirmed analytical inverse kinematics is not possible in MATLAB

--- Corke/AxisReloadedPoseCalc.m
+++ Corke/AxisReloadedPoseCalc.m
@@ -1,9 +1,9 @@
 function [Tab, Rab, Pab] = AxisReloadedPoseCalc(fingernum, theta)
-    
+
     Tsb = Tsbgen5000(theta);
-    Tas = [Rgamma(72*fingernum/180*pi), [0; 100; 0];[0, 0, 0], 1];
+    Tas = [Rgamma(72*fingernum/180*pi), [0; 100; 0]; [0, 0, 0], 1];
     Tab = Tas*Tsb;
     Rab = Tab(1:3, 1:3);
     Pab = Tab(1:3, 4);
-    
+
 end
--- Corke/axisInverseKinematics.m
+++ Corke/axisInverseKinematics.m
@@ -13,41 +13,38 @@ E = Tz(p1(3)) * Ry('q1') * Ty(p2(2)) * Rx('q2') * Tz(p3(3)) ...
 syms q1 q2 q3 real
 TE = E.fkine([q1, q2, q3]);
 
-% Power of Exponentials Method
+% Power of Exponentials Method -> Numerical
 %    Tsbgen5000([servoAngle1, servoAngle2, servoAngle3]) -> Claw tip pose in claw frame
 %    Rgamma(gamma) -> Calculates yaw rotation matrix
 %    Tsb -> Claw tip pose in claw frame
 %    Tab -> Center of disk to claw
 %    Tab -> Center of disk to end of claw pose
-
 for ii = 0:4
   Tsb = Tsbgen5000([0, 0, 0]);
   Tas = [Rgamma((72*ii)/180*pi), [-100*sind(72*ii); 100*cosd(72*ii); 0]; [0, 0, 0], 1];
   Tab{ii+1} = Tas*Tsb;
 end
 
-
-% % Symbolic Inverse Kinematics
-% syms t1 t2 t3 real
-% theta1 = [t1, t2, t3];
-% Tsb1 = Tsbgen5000(theta1);
-% Tas1 = [Rgamma(0), [0; 100; 0];[0, 0, 0], 1];
-% Tab1 = Tas1*Tsb1;
+% % Power of Exponentials Method (forward kinematics)-> Symbolic
+% syms j1 j2 j3 real
+% Tsb = Tsbgen5000([j1, j2, j3]);
+% Tas = [Rgamma(0), [-100*sind(0); 100*cosd(0); 0]; [0, 0, 0], 1];
+% Tab = simplify(Tas*Tsb);
 %
+% % Symbolic inverse kinematics
 % syms x y z real
-% e1 = x == Tab1(1, 4);
-% e2 = y == Tab1(2, 4);
-% e3 = z == Tab1(3, 4);
+% e1 = x == Tab(1, 4);
+% e2 = y == Tab(2, 4);
+% e3 = z == Tab(3, 4);
 %
-% S = solve([e1, e2, e3], [t1, t2, t3]);
+% S = solve([e1 e2 e3], [j1 j2 j3])
 
 % Numerical Inverse Kinematics
-
-theta0 = [0, 0, 0]; % initial guess
-pdes = [90; 100; 150];   % desired end effector position
-fkin = @(theta)(AxisReloadedPoseCalc(0, theta));    % function to compute pose of finger 0
-fpos = @(T)T(1:3, 4);   % function to extract position from pose
-err = @(theta) norm(fpos(fkin(theta)) - pdes);  % error between guess and desired
+theta0 = [0, 0, 0];                                % initial guess
+pdes = [90; 100; 150];                             % desired end effector position
+fkin = @(theta)(AxisReloadedPoseCalc(0, theta));   % function to compute pose of finger 0
+fpos = @(T)T(1:3, 4);                              % function to extract position from pose
+err = @(theta) norm(fpos(fkin(theta)) - pdes);     % error between guess and desired
 
 % fminsearch method
 options = optimset('TolFun', 0.001);
