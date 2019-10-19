45c920eb4bc4e7377754469bf3b92e3d2fcb6bf3
Riley <trd@ualberta.ca>
2019-10-18 18:58:49 -0600

    hacking some pausing in

--- EriClaw/main.py
+++ EriClaw/main.py
@@ -28,23 +28,27 @@ def main():
     # Queue for 
     inputQueue = queue.Queue()
     print("Startup complete.")
+    print("Input help or h for command list")
     inputThread = threading.Thread(target=read_kbd_input, args=(inputQueue,), daemon=True)
     inputThread.start()
-
+    see  = True
+    pause = False
     while (True):
-        correction = vc.getPosition()
-        state = vc.getState()
-        if state != "Centered":
-            correction = np.array([correction[0], correction[1], 0])
+        if see:
+            correction = vc.getPosition()
+            state = vc.getState()
+            if state != "Centered":
+                correction = np.array([correction[0], correction[1], 0])
             #print(correction)
-            mc.set_correction(correction)
-            mc.set_mode(2)
-        else:
-
-            mc.set_mode(1)
+                mc.set_correction(correction)
+                if not pause:
+                    mc.set_mode(2)
+            else:
+                if not pause:
+                    mc.set_mode(1)
 
         if (inputQueue.qsize() > 0):
-            input_str = inputQueue.get()
+            input_str = inputQueue.get().strip().lower()
             #print("input_str = {}".format(input_str))
 
             if (input_str == EXIT_COMMAND):
@@ -55,13 +59,34 @@ def main():
                 mc.set_mode(3)
                 print("Exiting serial terminal.")
                 break
+            elif pause and not input_str == 0:
+                print("System is paused, input 0 to resume")
             else:
                 if not input_str.isalpha():
                     mc.set_mode(int(input_str))
+                    if int(input_str) == 0:
+                        res = ''
+                        while not res == '0':
+                            res = input()
+                            print(res)
                 # print(vc.getPosition())
-                elif input_str == 's':
+                elif input_str == 'camera':
+                    camera = not camera
+                    camState = "Camera turned on." if camera else "Camera turned off."
+                    print(camState)
+                elif input_str == 'state' or input_str == 's':
                     print(state + " at : " +str(correction[0]) +" " + str(correction[1]))
-                elif input_str == 'recenter':
+                elif input_str == 'help' or input_str == 'h':
+                    print("Input Options:")
+                    print("camera will toggle the camera state")
+                    print("s or state returns the current position and state of the plate")
+                    print("recenter or c triggers a recenter motion after spin cycle completes")
+                    print("exit starts the shutdown sequence")
+                    print("0 pauses motion")
+                    print("1 resumes spin cycle")
+                    print("2 resumes recenter motion")
+                    print("3 is emergency limp (will drop plate)")
+                elif input_str == 'recenter' or input_str == 'c':
                     mc.set_mode(2)
                     mcState = mc.getMCState()
                     while mcState != 2:
